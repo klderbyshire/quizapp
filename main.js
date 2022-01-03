365bd774b08bc5a99b2b63fa32831dfc3ae1dd2a
Karen Derbyshire <Karen@Thomass-Mac-mini.local>
2022-01-03 15:54:39 +0000

    [More questions added]

--- main.js
+++ main.js
@@ -8,83 +8,105 @@ let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 nextButton.addEventListener('click', () => {
-    currentQuestionIndex++
-    setNextQuestion()
+  currentQuestionIndex++
+  setNextQuestion()
 })
 
 function startGame() {
-console.log('Started')
-startButton.classList.add('hide')
-shuffledQuestions = questions.sort(() => Math.random() - .5)
-currentQuestionIndex = 0
-questionContainerElement.classList.remove('hide')
-setNextQuestion()
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
 }
 
 function setNextQuestion() {
-    resetState()
-showQuestion(shuffledQuestions[currentQuestionIndex])
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function showQuestion(question) {
-questionElement.innertext = question.question
-question.answers.forEach(answer => {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
     const button = document.createElement('button')
-    button.innertext = answer.text
+    button.innerText = answer.text
     button.classList.add('btn')
     if (answer.correct) {
-        button.dataset.correct = answer.correct
+      button.dataset.correct = answer.correct
     }
     button.addEventListener('click', selectAnswer)
     answerButtonsElement.appendChild(button)
-})
+  })
 }
 
 function resetState() {
-    clearStatusClass(document.body)
-  nextButton.classList.add('hide')  
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
   while (answerButtonsElement.firstChild) {
-      answerButtonsElement.removeChild
-      (answerButtonsElement.firstChild)
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
   }
 }
 
 function selectAnswer(e) {
-    const selectedButton = e.target
-    const correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonsElement.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-    })
-    if (shuffledQuestions.length > currentQuestionIndex +1) {
-        nextButton.classList.remove('hide')
-     } else {
-         startButton.innerText = 'Reset'
-         startButton.classList.remove('hide')
-
-}}
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
 
 function setStatusClass(element, correct) {
-clearStatusClass(element)
-if (correct) {
+  clearStatusClass(element)
+  if (correct) {
     element.classList.add('correct')
- } else {
+  } else {
     element.classList.add('wrong')
-}
+  }
 }
 
-function clearStatusClass(element, correct) {
-    element.classList.remove('correct')
-    element.classList.remove('wrong')
-}  
-
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
 
 const questions = [
-    {
-        question: 'What is 2 + 2',
-        answers: [
-            { text: '4', correct: true },
-            { text: '22', correct: false }
-        ]
-    }
+  {
+    question: 'What is 2 + 2?',
+    answers: [
+      { text: '4', correct: true },
+      { text: '22', correct: false }
+    ]
+  },
+  {
+    question: 'What is 50 * 100?',
+    answers: [
+      { text: '5000', correct: true },
+      { text: '10000', correct: false },
+      { text: '100', correct: false },
+      { text: '500', correct: false }
+    ]
+  },
+  {
+    question: 'What is 20 * 4?',
+    answers: [
+      { text: '60', correct: false },
+      { text: '80', correct: true },
+      { text: '12', correct: false },
+      { text: '90', correct: false }
+    ]
+  },
+  {
+    question: 'What is 4 * 2?',
+    answers: [
+      { text: '6', correct: false },
+      { text: '8', correct: true }
+    ]
+  }
 ]
